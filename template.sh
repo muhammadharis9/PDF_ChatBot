b1ac5c9c83ef513bd5aa24dbc098b4fbbae70f65
muhammadharis <muhammadharis.imtiaz1@gmail.com>
2025-12-13 13:44:54 +0100

    Project template done

---
+++ template.sh
@@ -0,0 +1,12 @@
+mkdir -p src
+mkdir -p research 
+
+touch src/__init__.py
+touch src/helper.py
+touch .env
+touch .gitignore
+touch setup.py
+touch app.py 
+touch research/experiment.ipynb
+
+echo "Everything is done, ready to proceed!"
